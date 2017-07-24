3a8dedc561a149a7d3daec90c973083b2b5715ca
Vaniedson <vaniedsonsi@gmail.com>
2017-07-24 16:19:13 -0300

    commit16

--- src/View/Tela_Cadastros.java
+++ src/View/Tela_Cadastros.java
@@ -5,27 +5,41 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.SwingConstants;
 
-public class Tela_Cadastros extends JFrame{
+
+public class Tela_Cadastros extends JFrame implements ActionListener{
+	
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	JLabel prolnome, prolvalidade, prolpreco, prolquant_estoque, proldescricao;
+	JTextField profnome, profvalidade, profpreco, profquant_estoque, profdescricao;
+	JButton probcadastrar, probsair, problimpar;
 
+	JLabel clilnome, cliltipo, cliltelefone;
+	JTextField clifnome, cliftelefone;
+	JButton clibcadastrar, clibsair, cliblimpar;
+
+	JLabel selnome, seldescricao;
+	JTextField sefnome, sefdescricao;
+	JButton sebcadastrar, sebsair, seblimpar;
+	
+	JLabel fulnome, fulsexo, fulmatricula, fullogin, fulsenha, fulresenha;
+	JTextField fufnome, fufmatricula, fuflogin, fufsenha, fufresenha;
+	JButton fubcadastrar, fubsair, fublimpar;
 	
 	public Tela_Cadastros(){
 		super( "Tela de Cadastros" );
 		JTabbedPane tabbedPane = new JTabbedPane(); 
 
 		// TELA PRODUTOS 
-		JLabel prolnome, prolvalidade, prolpreco, prolquant_estoque, proldescricao;
-		JTextField profnome, profvalidade, profpreco, profquant_estoque, profdescricao;
-		JButton probcadastrar, probsair, problimpar;
-		
 		prolnome = new JLabel("NOME:");
 		profnome = new JTextField(25);
 		prolvalidade = new JLabel("VALIDADE:");
@@ -37,8 +51,12 @@ public class Tela_Cadastros extends JFrame{
 		proldescricao= new JLabel("DESCRIÇÃO:");
 		profdescricao = new JTextField(25);
 		probcadastrar = new JButton("CADASTRAR");
+		probcadastrar.addActionListener(this);
 		probsair = new JButton("SAIR");
+		probsair.addActionListener(this);
 		problimpar = new JButton("LIMPAR");
+		problimpar.addActionListener(this);
+
 
 		JLabel label1 = new JLabel( "", SwingConstants.CENTER );
 		JPanel panel1 = new JPanel(); 
@@ -56,18 +74,15 @@ public class Tela_Cadastros extends JFrame{
 		panel1.add(probcadastrar);
 		panel1.add(probsair);
 		panel1.add(problimpar);
-		
+
 		panel1.add( label1 );
 		tabbedPane.addTab( "PRODUTO", null, panel1, "First Panel" ); 
 
 
-		
+
 
 		// TELA CLIENTE
-		JLabel clilnome, cliltipo, cliltelefone;
-		JTextField clifnome, cliftelefone;
-		JButton clibcadastrar, clibsair, cliblimpar;
-		
+
 		JComboBox clicombo = new JComboBox(new Object[]{"              PESSOA FÍSICA           ", "          PESSOA JURÍDICA        "});
 		clilnome = new JLabel("               NOME:               ");
 		clifnome = new JTextField(25);
@@ -75,12 +90,15 @@ public class Tela_Cadastros extends JFrame{
 		cliltelefone = new JLabel("TELEFONE:");
 		cliftelefone = new JTextField(25);
 		clibcadastrar = new JButton("CADASTRAR");
+		clibcadastrar.addActionListener(this);
 		clibsair = new JButton("SAIR");
+		clibsair.addActionListener(this);
 		cliblimpar = new JButton("LIMPAR");
-		
+		cliblimpar.addActionListener(this);
+
 		JLabel label2 = new JLabel( "", SwingConstants.CENTER );
 		JPanel panel2 = new JPanel(); 
-		
+
 		panel2.add(cliltipo);
 		panel2.add(clicombo);
 		panel2.add(clilnome);
@@ -90,29 +108,29 @@ public class Tela_Cadastros extends JFrame{
 		panel2.add(clibcadastrar);
 		panel2.add(clibsair);
 		panel2.add(cliblimpar);
-		
+
 		panel2.add( label2 ); 
 		tabbedPane.addTab( "CLIENTE", null, panel2, "Second Panel" );
-		
 
-		
-		
+
+
+
 		// TELA SESSÃO
-		JLabel selnome, seldescricao;
-		JTextField sefnome, sefdescricao;
-		JButton sebcadastrar, sebsair, seblimpar;
-		
+
 		selnome = new JLabel("NOME:");
 		sefnome = new JTextField(25);
 		seldescricao = new JLabel("DESCRIÇÃO:");
 		sefdescricao = new JTextField(25);
 		sebcadastrar = new JButton("CADASTRAR");
+		sebcadastrar.addActionListener(this);
 		sebsair = new JButton("SAIR");
+		sebsair.addActionListener(this);
 		seblimpar = new JButton("LIMPAR");
-		
+		seblimpar.addActionListener(this);
+
 		JLabel label3 = new JLabel( "", SwingConstants.CENTER );
 		JPanel panel3 = new JPanel(); 
-		
+
 		panel3.add(selnome);
 		panel3.add(sefnome);
 		panel3.add(seldescricao);
@@ -120,18 +138,15 @@ public class Tela_Cadastros extends JFrame{
 		panel3.add(sebcadastrar);
 		panel3.add(sebsair);
 		panel3.add(seblimpar);
-		
+
 		tabbedPane.addTab( "SESSÃO", null, panel3, "Third Panel" );
-		
-		
+
+
 
 		//TELA FUNCIONÁRIO
-		JLabel fulnome, fulsexo, fulmatricula, fullogin, fulsenha, fulresenha;
-		JTextField fufnome, fufmatricula, fuflogin, fufsenha, fufresenha;
-		JButton fubcadastrar, fubsair, fublimpar;
-		
+
 		JComboBox fucombo = new JComboBox(new Object[]{"               MASCULINO               ", "               FEMININO               "});
-		
+
 		fulsexo = new JLabel("               SEXO:               ");
 		fulnome = new JLabel("               NOME:               ");
 		fufnome = new JTextField(25);
@@ -144,12 +159,15 @@ public class Tela_Cadastros extends JFrame{
 		fulresenha = new JLabel("CONFIRMAR A SENHA:");
 		fufresenha = new JTextField(25);
 		fubcadastrar = new JButton("CADASTRAR");
+		fubcadastrar.addActionListener(this);
 		fubsair = new JButton("SAIR");
+		fubsair.addActionListener(this);
 		fublimpar = new JButton("LIMPAR");
-		
+		fublimpar.addActionListener(this);
+
 		JLabel label4 = new JLabel( "", SwingConstants.CENTER );
 		JPanel panel4 = new JPanel(); 
-		
+
 		panel4.add(fulsexo);
 		panel4.add(fucombo);
 		panel4.add(fulnome);
@@ -165,11 +183,72 @@ public class Tela_Cadastros extends JFrame{
 		panel4.add(fubcadastrar);
 		panel4.add(fubsair);
 		panel4.add(fublimpar);
-		
+
 		panel4.add( label4 );
 		tabbedPane.addTab( "FUNCIONÁRIO", null, panel4, "Second Panel" );
-		
+
 		add( tabbedPane );
-	} 
+		
+	}
+
+		
+	public void actionPerformed(ActionEvent e) {
+		// AÇÃO PRODUTO
+		if(e.getSource().equals(this.probcadastrar)) {
+			//METODO CADASTRAR
+			dispose();
+		}
+		if (e.getSource().equals(this.probsair)) {
+			dispose();
+			new Tela_Menu();
+		}
+		if (e.getSource().equals(this.problimpar)) {
+			dispose();
+			// COMANDO LIMPAR JTEXTFIELD
+		}
+		
+		//AÇÃO CLIENTE
+		if(e.getSource().equals(this.clibcadastrar)) {
+			//METODO CADASTRAR;
+			dispose();
+		}
+		if (e.getSource().equals(this.clibsair)) {
+			dispose();
+			new Tela_Menu();
+		}
+		if (e.getSource().equals(this.cliblimpar)) {
+			dispose();
+			// COMANDO LIMPAR JTEXTFIELD
+		}
+		
+		//AÇÃO SESSÃO
+		if(e.getSource().equals(this.sebcadastrar)) {
+			//METODO CADASTRAR
+			dispose();
+		}
+		if (e.getSource().equals(this.sebsair)) {
+			dispose();
+			new Tela_Menu();
+		}
+		if (e.getSource().equals(this.seblimpar)) {
+			dispose();
+			// COMANDO LIMPAR JTEXTFIELD
+		}
+		
+		//AÇÃO FUNCIONÁRIO
+		if(e.getSource().equals(this.fubcadastrar)) {
+			//METODO CADASTRAR
+			dispose();
+		}
+		if (e.getSource().equals(this.fubsair)) {
+			dispose();
+			new Tela_Menu();
+		}
+		if (e.getSource().equals(this.fublimpar)) {
+			dispose();
+			// COMANDO LIMPAR JTEXTFIELD
+		}
+	}
+
 } 
 
